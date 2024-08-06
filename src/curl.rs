abfcaaf63fae86a6ae03c6396e649dc9d5a4e504
diogofriggo <diogofriggobr@gmail.com>
2024-08-06 13:03:57 +0300

    unix support

--- src/curl.rs
+++ src/curl.rs
@@ -1,5 +1,6 @@
 use std::fs::{self, OpenOptions};
 use std::io::Write;
+#[cfg(windows)]
 use std::os::windows::process::CommandExt as _;
 use std::process::{Command, Output};
 
@@ -11,7 +12,7 @@ pub fn get(url: String) -> Result<String, MtcapError> {
     let response = Command::new("curl")
         .arg("-k")
         .arg(url)
-        .creation_flags(CREATE_NO_WINDOW)
+        .os_aware_creation_flags(CREATE_NO_WINDOW)
         .output()?;
 
     response_analyse(&response)?;
@@ -27,7 +28,7 @@ pub fn post(url: String) -> Result<(), MtcapError> {
         .arg("POST")
         .arg("-d")
         .arg("\"\"")
-        .creation_flags(CREATE_NO_WINDOW)
+        .os_aware_creation_flags(CREATE_NO_WINDOW)
         .output()?;
 
     response_analyse(&response)?;
@@ -53,7 +54,7 @@ pub fn put(url: String, json: json::JsonValue) -> Result<(), MtcapError> {
         .arg(format!("@{}", FILE_NAME))
         .arg("-H")
         .arg("Content-Type: application/json")
-        .creation_flags(CREATE_NO_WINDOW)
+        .os_aware_creation_flags(CREATE_NO_WINDOW)
         .output();
 
     let _ = fs::remove_file(FILE_NAME);
@@ -69,7 +70,7 @@ pub fn delete(url: String) -> Result<(), MtcapError> {
         .arg(url)
         .arg("-X")
         .arg("DELETE")
-        .creation_flags(CREATE_NO_WINDOW)
+        .os_aware_creation_flags(CREATE_NO_WINDOW)
         .output()?;
 
     response_analyse(&response)?;
@@ -87,5 +88,20 @@ fn response_analyse(output: &Output) -> Result<(), MtcapError> {
         Err(MtcapError::Other(json["error"].to_string()))
     } else {
         Err(MtcapError::Other(format!("{:?}", output)))
+    }
+}
+
+trait CreationFlagsBuilderSetting {
+    fn os_aware_creation_flags(&mut self, flag: u32) -> &mut Self;
+}
+
+impl CreationFlagsBuilderSetting for Command {
+    fn os_aware_creation_flags(&mut self, _flag: u32) -> &mut Self {
+        #[cfg(windows)]
+        self.creation_flags(_flag);
+        // creation flags are windows-specific
+        // CREATE_NO_WINDOW instructs windows not to create a console window while running the
+        // desired executable (curl.exe in this case, not installed by default on windows)
+        self
     }
 }
